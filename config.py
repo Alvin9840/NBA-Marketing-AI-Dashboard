73fe0ac594038d7512e851540b55a8650c59bbd1
Sudip <sudip.manchare@gmail.com>
2025-11-12 14:31:47 -0500

    removed yaml files and added config template

---
+++ config.py
@@ -0,0 +1,12 @@
+WATSONX_API_KEY = "your-api-key-here"
+WATSONX_URL = "https://us-south.ml.cloud.ibm.com/"
+WATSONX_PROJECT_ID = "your-project-id-here"
+
+COORDINATOR_ID = "ibm/granite-3-8b-instruct"
+COORDINATOR_PARAMETERS = {
+    "decoding_method": "greedy",
+    "max_new_tokens": 2000,
+    "temperature": 0.1,
+    "top_p": 0.9,
+    "top_k": 50
+}
